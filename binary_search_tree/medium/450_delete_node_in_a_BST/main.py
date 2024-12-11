ea868147aa122885e6738682b0e34125dd421e43
Shigeo Kitamura <shigeo9462@gmail.com>
2024-12-11 13:18:00 -0800

    add 450

---
+++ binary_search_tree/medium/450_delete_node_in_a_BST/main.py
@@ -0,0 +1,55 @@
+# https://leetcode.com/problems/delete-node-in-a-bst/description/?envType=study-plan-v2&envId=leetcode-75
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                node = root.right
+                while node.left:
+                    node = node.left
+
+                root.val = node.val
+                root.right = self.deleteNode(root.right, node.val)
+
+        return root
+
+if __name__ == "__main__":
+    s = Solution()
+
+    root = TreeNode(5)
+    root.left = TreeNode(3)
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(4)
+    root.right = TreeNode(6)
+    root.right.right = TreeNode(7)
+    s.deleteNode(root, 3)
+
+    root = TreeNode(5)
+    root.left = TreeNode(3)
+    root.right = TreeNode(6)
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(4)
+    root.right.right = TreeNode(7)
+    s.deleteNode(root, 0)
+
+    root = None
+    s.deleteNode(root, 0)
